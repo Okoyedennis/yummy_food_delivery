76f909d00cad7d6143c17ba63110ac284bf7881c
Dennis <okoyedennis7@gmail.com>
2022-09-29 21:22:04 +0100

    first commit

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -43,22 +43,22 @@ function Footer() {
           <h1 className="title">FIND OUR RESTAURANTS</h1>
           <p className="text">
             1654 R. Don Road #304.
-            <br /> NewYork, 85022
+            <br /> Lagos, 85022
             <br /> (602) 867-1010
           </p>
           <p className="text">
             2356 K. Laquie Rd #235.
-            <br /> NewYork, 85022
+            <br /> Abuja, 85022
             <br /> (602) 867-1011
           </p>
           <p className="text">
             1614 E. Erwin St #104.
-            <br /> NewYork, 85022
+            <br /> Lagos, 85022
             <br /> (602) 867-1012
           </p>
           <p className="text">
             1614 W. Caroll St #125.
-            <br /> NewYork, 85022
+            <br /> Abuja, 85022
             <br /> (602) 867-1013
           </p>
         </div>
